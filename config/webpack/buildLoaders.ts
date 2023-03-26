4eb3ec906afc906b3a6b13f9c12e3825dec09a45
Art <ya.Artriches@ya.ru>
2023-03-26 12:48:26 +0300

    refactoring

--- config/webpack/buildLoaders.ts
+++ config/webpack/buildLoaders.ts
@@ -3,30 +3,12 @@ import webpack from "webpack";
 import { IBuildOptions } from "./types/config";
 import { buildCssLoader } from "./loaders/buildCssLoader";
 import { buildSvgLoader } from "./loaders/buildSvgLoader";
+import { buildBabelLoader } from "./loaders/buildBabelLoader";
 
 export function buildLoaders({ isDev }: IBuildOptions): webpack.RuleSetRule[] {
 	const svgLoader = buildSvgLoader();
 
-	const babelLoader = {
-		test: /\.(js|jsx|tsx)$/,
-		exclude: /node_modules/,
-		use: {
-			loader: "babel-loader",
-			options: {
-				presets: ["@babel/preset-env"],
-				plugins: [
-					[
-						"i18next-extract",
-						{
-							locales: ["ru", "en"],
-							keyAsDefaultValue: true,
-						},
-					],
-				],
-			},
-		},
-	};
-
+	const babelLoader = buildBabelLoader();
 	const cssLoader = buildCssLoader(isDev);
 
 	// Если не используем тайпскрипт - нужен babel-loader
---
+++ config/webpack/loaders/buildBabelLoader.ts
@@ -0,0 +1,21 @@
+export function buildBabelLoader() {
+	return {
+		test: /\.(js|jsx|tsx)$/,
+		exclude: /node_modules/,
+		use: {
+			loader: "babel-loader",
+			options: {
+				presets: ["@babel/preset-env"],
+				plugins: [
+					[
+						"i18next-extract",
+						{
+							locales: ["ru", "en"],
+							keyAsDefaultValue: true,
+						},
+					],
+				],
+			},
+		},
+	};
+}
